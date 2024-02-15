bebecc4182ae9b11c2cf4d9b86e298f97d9a470d
ThienMenh <dinhthienmenh1505@gmail.com>
2024-02-15 18:25:19 +0700

    add feature:  product filter

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Eshopper.Data;
 using Eshopper.Models;
 using Eshopper.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Eshopper.Controllers
 {
@@ -19,6 +20,38 @@ namespace Eshopper.Controllers
         {
             _context = context;
         }
+        public class PriceRange
+        {
+            public int Min {get; set;}
+            public int Max {get; set;}
+        }
+        [HttpPost]
+        public IActionResult GetFilteredProducts([FromBody] FilterData filter)
+        {
+            var filteredProducts = _context.Products.ToList();
+            if(filter.PriceRanges != null && filter.PriceRanges.Count > 0 && !filter.PriceRanges.Contains("all"))
+            {
+                List<PriceRange> priceRanges = new List<PriceRange>();
+                foreach(var range in filter.PriceRanges)
+                {
+                    var value = range.Split("-").ToArray();
+                    PriceRange priceRange = new PriceRange();
+                    priceRange.Min = Int16.Parse(value[0]);
+                    priceRange.Max = Int16.Parse(value[1]);
+                    priceRanges.Add(priceRange);
+                }
+                filteredProducts = filteredProducts.Where(p => priceRanges.Any(r =>p.ProductPrice >= r.Min && p.ProductPrice <= r.Max)).ToList();
+            }
+            if (filter.Colors != null && filter.Colors.Count > 0 && !filter.Colors.Contains("all"))
+            {
+                filteredProducts = filteredProducts.Where(p => filter.Colors.Contains(p.color.ColorName)).ToList();
+            }
+            if (filter.Sizes != null && filter.Sizes.Count > 0 && !filter.Sizes.Contains("all"))
+            {
+                filteredProducts = filteredProducts.Where(p => filter.Sizes.Contains(p.size.SizeName)).ToList();
+            }
+                return PartialView("_ReturnProducts", filteredProducts);
+        }
         public int PageSize = 9;
         // GET: Products
         public async Task<IActionResult> Index(int productPage = 1)
@@ -27,6 +60,25 @@ namespace Eshopper.Controllers
                 new ProductListViewModel
                 {
                     Products = _context.Products.Skip((productPage - 1) * PageSize).Take(PageSize),
+                    pagingInfo = new PagingInfo
+                    {
+                        ItemsPerPage = PageSize,
+                        CurrentPage = productPage,
+                        TotalItems = _context.Products.Count()
+                    }
+                }
+            );
+        }
+        [HttpPost]
+        public async Task<IActionResult> Search(string keywords, int productPage = 1)
+        {
+            return View("Index",
+                new ProductListViewModel
+                {
+                    Products = _context.Products
+                    .Where(p => p.ProductName.Contains(keywords))
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
                     pagingInfo = new PagingInfo
                     {
                         ItemsPerPage = PageSize,
@@ -62,7 +114,7 @@ namespace Eshopper.Controllers
 
             return View(product);
         }
-
+        [Authorize]
         // GET: Products/Create
         public IActionResult Create()
         {
@@ -77,6 +129,7 @@ namespace Eshopper.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Create([Bind("ProductId,ProductName,ProductDescription,CategoryId,ProductPrice,ProductDiscount,ProductPhoto,SizeId,ColorId,IsTrandy,IsArrived")] Product product)
         {
             if (ModelState.IsValid)
@@ -90,7 +143,7 @@ namespace Eshopper.Controllers
             ViewData["SizeId"] = new SelectList(_context.Sizes, "SizeId", "SizeId", product.SizeId);
             return View(product);
         }
-
+        [Authorize]
         // GET: Products/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -115,6 +168,7 @@ namespace Eshopper.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,ProductDescription,CategoryId,ProductPrice,ProductDiscount,ProductPhoto,SizeId,ColorId,IsTrandy,IsArrived")] Product product)
         {
             if (id != product.ProductId)
@@ -147,7 +201,7 @@ namespace Eshopper.Controllers
             ViewData["SizeId"] = new SelectList(_context.Sizes, "SizeId", "SizeId", product.SizeId);
             return View(product);
         }
-
+        [Authorize]
         // GET: Products/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -172,6 +226,7 @@ namespace Eshopper.Controllers
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var product = await _context.Products.FindAsync(id);
---
+++ Models/FilterData.cs
@@ -0,0 +1,9 @@
+namespace Eshopper.Models
+{
+    public class FilterData
+    {
+        public List<string> PriceRanges {get; set;}
+        public List<string> Colors {get; set;}
+        public List<string> Sizes {get; set;}
+    }
+}
